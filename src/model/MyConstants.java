4c613104a692f6a67b0c32e124413d2050008d96
Boris <boris@telemessage.com>
2021-09-16 10:06:38 +0300

    change in db link

--- src/model/MyConstants.java
+++ src/model/MyConstants.java
@@ -7,7 +7,7 @@ public class MyConstants {
     public static final String BOT_TOKEN = "BOT TOKEN HERE";
     public static final String BOT_USERNAME = "Travel_Bot";
     public static final int ADMIN_ID = 243895329;
-    public static final String DB_url = "jdbc:mysql://localhost:3306/travelbase?autoReconnect=true&useSSL=false";
+    public static final String DB_url = "jdbc:mysql://127.0.0.1:3306/travelbase?autoReconnect=true&useSSL=false&characterEncoding=UTF-8";
     public static final String DB_username = "root";
     public static final String DB_password = "Pa$$w0rd";
 }
