596bfd6e9aa30bd823d1546c656dbd097744c938
Boris <boris@telemessage.com>
2025-03-21 16:33:41 +0200

    updated to new telegram lib

--- src/model/HttpHelper.java
+++ src/model/HttpHelper.java
@@ -9,13 +9,71 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class HttpHelper {
     static private HashMap<String, Tuple<Double, Date>> cash = new HashMap<>();
 
     public static double convertCurrency(double amount, String from, String to) throws Exception{
+        Date validityDate = new Date(System.currentTimeMillis() - (24 * 60 * 60 * 1000));
+
+        if (cash.containsKey(from+to)) {
+            if (cash.get(from + to).y.after(validityDate)) {
+                return cash.get(from + to).x * amount;
+            } else{
+                cash.remove(from+to);
+            }
+        }
+
+        String urlString = "https://v6.exchangerate-api.com/v6/" + MyConstants.ExchangerateApiKey + "/latest/" + from;
+
+        URL url = new URL(urlString);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        // Create an ObjectMapper instance
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        // Parse the JSON string
+        JsonNode jsonNode = objectMapper.readTree(response.toString());
+
+        // Get the 'conversion_rates' node
+        JsonNode conversionRatesNode = jsonNode.path("conversion_rates");
+
+        // Iterate over the conversion rates
+        Iterator<Map.Entry<String, JsonNode>> fields = conversionRatesNode.fields();
+        while (fields.hasNext()) {
+            Map.Entry<String, JsonNode> field = fields.next();
+            String currencyCode = field.getKey();
+            double conversionRate = field.getValue().asDouble();
+
+            if (!currencyCode.equals(from) && conversionRate > 0.001){
+                cash.put(from + currencyCode, new Tuple<>(conversionRate, new Date(System.currentTimeMillis())));
+            }
+        }
+
+        if (cash.containsKey(from+to)) {
+            return cash.get(from + to).x * amount;
+        }else{
+            throw new IOException("exchange from " + from + " to " + to + " not found on v6.exchangerate-api.com");
+        }
+    }
+
+    public static double convertCurrencyOld(double amount, String from, String to) throws Exception{
         Date validityDate = new Date(System.currentTimeMillis() - (60 * 60 * 1000));
 
         if (cash.containsKey(from+to)) {
